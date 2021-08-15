e7ffba20aeec5dd030a2ae4b0872446a6c9fab32
Vitalii Stikhurov <lemonograb@yandex-team.ru>
2021-08-16 00:19:57 +0300

    s

--- MediaHack-Server/Sources/App/routes.swift
+++ MediaHack-Server/Sources/App/routes.swift
@@ -9,7 +9,8 @@ func routes(_ app: Application) throws {
         return "Hello, world!"
     }
 
-    app.get("film") { request in
-        return Response("https://1a5af214-d010-4ccc-b666-eba61130c0db.ams-static-03.cdntogo.net/pd/aWQ9NTM0OTA3OzE4NDUyODMxNDE7MTIzODM4Nzg7MTYyOTA2MTk0NCZoPXQtSEIyRG9UOVFZOGdVa3pPMWJvM0EmZT0xNjI5MTQ4MzQ0/9/5f/UShR5jRxtgmLNkVXY.mp4")
+    app.get("film") { req in
+        return
+            req.redirect(to: "https://1a5af214-d010-4ccc-b666-eba61130c0db.ams-static-03.cdntogo.net/pd/aWQ9NTM0OTA3OzE4NDUyODMxNDE7MTIzODM4Nzg7MTYyOTA2MTk0NCZoPXQtSEIyRG9UOVFZOGdVa3pPMWJvM0EmZT0xNjI5MTQ4MzQ0/9/5f/UShR5jRxtgmLNkVXY.mp4", type: .permanent)
     }
 }
